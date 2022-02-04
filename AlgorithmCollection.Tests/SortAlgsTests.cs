dd6ee1fb202dba78662fe3658f4bf89059722700
Oleg Naraevskiy <noa.oleg96@gmail.com>
2022-02-04 11:53:41 +0300

    * Fix filespaces and names + Add XUnit project

--- AlgorithmCollection.Tests/SortAlgsTests.cs
+++ AlgorithmCollection.Tests/SortAlgsTests.cs
@@ -6,19 +6,9 @@
 
 using System;
 
-namespace SearchAlgorithms
+namespace AlgorithmCollection.Tests
 {
-	public class BinarySearchAlg
+	public class SortAlgsTests
 	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-
-		public void SearchElement(int[] IntArray)
-		{
-			//int low, high = 0;
-			//var sortArray = IntArray.So
-		}
 	}
 }
